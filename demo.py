aef9f350a37df177de42c5f3480e267a5b828ecb
VaradharajMurugesan <varadharaj@emergertech.com>
2023-05-08 16:57:07 +0530

    commit

--- demo.py
+++ demo.py
@@ -1,34 +1,6 @@
-from flask import Flask, request, jsonify
-import sqlite3
-import json
-import mysql.connector
-from datetime import datetime
-from flask_cors import CORS
-
-
+from flask import Flask
 app = Flask(__name__)
-CORS(app)
 
-
-token_id = None
-
-@app.route('/validate_token',methods=['GET'])
-def validate_token():
-    print("intiating")
-    global token_id
-    try:
-        message = None
-        args = request.args
-        token_id=args.get("tokenID")        
-        #set_processcompleted()       
-        return jsonify("message: ","Success")
-    
-    except Exception as e:
-        print(e,"Something error in Posting")
-        return jsonify("Something wrong")
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
+@app.route("/")
+def index():
+    return "<h1>Hello Azure!</h1>"
